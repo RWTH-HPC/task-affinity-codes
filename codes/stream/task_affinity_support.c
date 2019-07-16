2df93342057de1c6c1984082c8fc5c3cda8bae9a
Robert Rudnev <robert.rudnev@rwth-aachen.de>
2019-07-16 18:39:32 +0200

    fixes

--- codes/stream/task_affinity_support.c
+++ codes/stream/task_affinity_support.c
@@ -17,7 +17,7 @@ int init_task_affinity()
 #ifdef TASK_AFFINITY
     kmp_affinity_thread_selection_mode_t thread_selection_strategy = get_env_int_value(kmp_affinity_thread_selection_mode_random, "THREAD_SELECTION_STRATEGY");
     kmp_affinity_map_mode_t affinity_map_mode = get_env_int_value(kmp_affinity_map_type_domain, "AFFINITY_MAP_MODE");
-    kpm_affintiy_page_selection_strategy_t page_selection_strategy = get_env_int_value(kmp_affinity_page_mode_divide_in_n_pages, "PAGE_SELECTION_MODE");
+    kmp_affintiy_page_selection_strategy_t page_selection_strategy = get_env_int_value(kmp_affinity_page_mode_divide_in_n_pages, "PAGE_SELECTION_MODE");
     kmp_affinity_page_weighting_strategy_t page_weighting_strategy = get_env_int_value(kmp_affinity_page_weight_mode_majority, "PAGE_WEIGHTING_STRATEGY");
     int number_of_affinities = get_env_int_value(1, "NUMBER_OF_AFFINITIES");
 
